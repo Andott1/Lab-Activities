fb5a64bf1a6081334705cb773679c7e5af848d14
Andott1 <145018255+Andott1@users.noreply.github.com>
2023-09-14 05:10:14 -0700

    Add files via upload

---
+++ Olaer.py
@@ -0,0 +1,9 @@
+# This code prints the following information:
+# Complete Name, Course, Section, Age, Birthday, and Complete Home Address
+
+print("Kurt Andre L. Olaer")
+print("Bachelor of Science in Computer Science")
+print("CS1D")
+print("19")
+print("August 31, 2004")
+print("La Buena Vida Gran Europa Lumbia Cagayan de Oro City")
